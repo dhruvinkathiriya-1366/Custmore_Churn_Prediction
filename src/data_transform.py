3efc01187f8447e33c5e4fb6e211705a27c79886
dhruvin kathiriya <dhruvinkathiriya1366@gmail.com>
2026-07-01 17:10:24 +0530

    add scaler and imrove

--- src/data_transform.py
+++ src/data_transform.py
@@ -1,42 +1,44 @@
-from data_ingestion import load_data,clean_data,split_data
+from data_ingestion import load_data,clean_data,fetch_column,split_data
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import OneHotEncoder,OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder,OrdinalEncoder,RobustScaler,StandardScaler
 from sklearn.compose import ColumnTransformer
 
 
 data_df=load_data()
 data_clean_df=clean_data(data_df)
 x_train,x_test,y_train,y_test=split_data(data_clean_df)
+ordinal_col,nominal_col,robust_col,standard_col=fetch_column(x_train)
 
-def Transform_data(data_clean_df):
+def Transform_data(ordinal_col,nominal_col,robust_col,standard_col):
     
-    cat_col=data_clean_df.select_dtypes(include="object")
-    ordinal_col=['Contract']
-    nominal_col=[col for col in cat_col if col not in ordinal_col]
-    
-    Ordinal_transform=Pipeline([
+     Ordinal_pipeline=Pipeline([
         ('imputetion',SimpleImputer(strategy="most_frequent")),
-        ('ordinaltransform',OrdinalEncoder(),)
+        ('ordinaltransform',OrdinalEncoder())
         ]) 
-    
-    onehot_transformer=Pipeline([
+     
+     onehot_pipeline=Pipeline([
          ('imputetion',SimpleImputer(strategy="most_frequent")),
          ('onehottransform',OneHotEncoder(handle_unknown="infrequent_if_exist",min_frequency=2))
     ])
-    
-    Transformer=ColumnTransformer(
+     
+     Transformer=ColumnTransformer(
         transformers=(
-        ["ordinal",Ordinal_transform,ordinal_col],
-        ["onehot",onehot_transformer,nominal_col]
+        ["ordinal",Ordinal_pipeline,ordinal_col],
+        ["onehot",onehot_pipeline,nominal_col],
+        ['Sscale',StandardScaler(),standard_col],
+        ['Rscale',RobustScaler(),robust_col]
         ),
         remainder="passthrough"
         )
+     return Transformer
     
-    return Transformer
-    
-if __name__ =="main":
+if __name__ =="__main__":
     data_df=load_data()
     data_clean_df=clean_data(data_df)
     x_train,x_test,y_train,y_test=split_data(data_clean_df)
-    Transform_data(x_train,x_test,y_train,y_test)
+    ordinal_col,nominal_col,robust_col,standard_col=fetch_column(x_train)
+    transformer=Transform_data(ordinal_col,nominal_col,robust_col,standard_col)
+    x_train_tran = transformer.fit_transform(x_train)
+    
+            
