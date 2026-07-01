334ff13004f88b8124ee7bac52b97066cacbe1a9
dhruvin kathiriya <dhruvinkathiriya1366@gmail.com>
2026-07-01 17:13:50 +0530

    removenumpy

--- src/model.py
+++ src/model.py
@@ -1,19 +1,1 @@
-# Model Definition Module
-# Model architecture and utility functions
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
-
-class ChurnModel:
-    def __init__(self):
-        """Initialize the churn prediction model"""
-        self.model = RandomForestClassifier(n_estimators=100)
-        self.scaler = StandardScaler()
-    
-    def train(self, X_train, y_train):
-        """Train the model"""
-        self.model.fit(X_train, y_train)
-    
-    def predict(self, X_test):
-        """Make predictions"""
-        return self.model.predict(X_test)
